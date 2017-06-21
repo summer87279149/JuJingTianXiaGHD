5fc4a1d54722ddd1a64baf82939110323cdf5d86
Admin <1033948449@qq.com>
2017-06-21 10:13:41 +0800

    create project

---
+++ JuJingTianXiaGHD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JuJingTianXiaGHD
+//
+//  Created by Admin on 2017/6/21.
+//  Copyright © 2017年 Summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JuJingTianXiaGHD/AppDelegate.m
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.m
+//  JuJingTianXiaGHD
+//
+//  Created by Admin on 2017/6/21.
+//  Copyright © 2017年 Summer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+    return YES;
+}
+
+
+
+
+
+
+
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+   
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    
+}
+
+
+@end
